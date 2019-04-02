809c82974b49356e039a320e287fbda1b98a2740
Thomaslundy1 <48457904+Thomaslundy1@users.noreply.github.com>
2019-04-02 14:15:50 +1100

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main(){
-
-printf ("Enter statement to be encrypted: ");
-//added a comment
+  int main()
+{
+  printf ("Enter statement to be encrypted: ");
+  scanf ("Enter key"); 
+  //added a comment
   return 0;
 }
