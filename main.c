2fa8ae5361c9f3459a4df638c7fc179319b0a14d
Thomaslundy1 <48457904+Thomaslundy1@users.noreply.github.com>
2019-04-09 12:09:39 +1000

    Update main.c

--- main.c
+++ main.c
@@ -2,19 +2,23 @@
 
   int main()
 {
-    int option;
-  printf ("Encrypt as rotation or substitution cypher?:\n ");
+    char option;
+    int ms1, key;
+  printf (" Encrypt as rotation or substitution cypher?:\n ");
    printf("\t1 for rotation\n \t2 for substitution\n");
     scanf("%c", &option);
     
    switch (option)
     {
-        case'1':
+        case'1': //rotation cipher
         printf ("What message to encrypt as a rotation: ");
+        scanf("%d", &ms1);
+        printf ("Please enter key for encryption: ");
+        scanf("%d", &key);
         break;
         
-        case'2':
-        printf ("What message to encrypt as a rotation: ");
+        case'2': //substitution cipher
+        printf ("What message to encrypt as a substitution: ");
         break;
         
         default:
