1aebd640fcb468b1c4535ce9e0fddebb85aa7945
Thomaslundy1 <48457904+Thomaslundy1@users.noreply.github.com>
2019-04-16 10:05:24 +1000

    Update main.c

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
   int main()
 {
    printf ("\t What would you like to do?:\n ");
-   printf("\t 1. Rotation cypher encription\n");
+   printf("\t 1. Rotation cypher encryption\n");
    printf("\t 2. Substitution cypher encryption\n");
    printf("\t 3. Rotation cypher decryption\n");
    printf("\t 4. Substitution cypher decryption\n");
@@ -13,9 +13,12 @@
     {
         case'1': //rotation cipher
         printf ("What message to encrypt as a rotation: ");
-        scanf("%c", &ms1);
+        scanf("%s", &ms1);
         printf ("Please enter key for encryption: ");
         scanf("%c", &key);
+        for(n = 0; n < 1024 && ms1[n] != '\0'; n++)
+        ms1[n] = ms1[n] + key;
+        printf("The message is: %s", ms1);
         break;
         
         case'2': //substitution cipher
